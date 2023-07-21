78d8930a0990cc1eddac2108a21f6916af00712a
Vwore <v.wore@iitg.ac.in>
2023-07-21 22:32:25 +0530

    edit button complete

--- todo.js
+++ todo.js
@@ -1,11 +1,13 @@
 
-	var curid=1;
+    var curid=1;
     var tasks= new Map();
     const forms=document.getElementById("addform");
     var data=document.getElementById('user_name');
     console.log(forms.one.value);
     var y=1;
     forms.addEventListener('submit',submits);
+
+
 
 function submits(e){
 	e.preventDefault();
@@ -18,12 +20,29 @@ function submits(e){
     }
 }
 
-function delete_task(id){
+function edit_submit(e){
+    e.preventDefault();
+    var x=document.getElementById('editinput');
+    // console.log(e.target.parentElement.id);
+    tasks.set((Number)(e.target.parentElement.id),x.value);
+    rerender();
+}
 
-   console.log(tasks.delete(Number(id)));
+function delete_task(id){
+    // console.log(id);
+   console.log(tasks.delete((Number)(id)));
    console.log(tasks.size);
    rerender();
 }
+function edit(id)
+{
+    // console.log('edit '+id);
+    var x=document.getElementById(id);
+    x.innerHTML="<form id='editform'><label>Enter the event</label><input id='editinput'></input><input type='submit' value='SAVE' style='background-color: rgb(4, 105, 114); height: 35px; border-radius: 20px; color: white;' ></form>"; 
+    var edit_task=document.getElementById('editform');
+    edit_task.addEventListener('submit',edit_submit);
+}
+
 function create_task(value,key)
 {
     
@@ -32,14 +51,27 @@ function create_task(value,key)
     var para= document.createElement("p");
     var text= document.createTextNode(value);
     para.className="task_text";
+    para.id=key+"1";
     para.appendChild(text);
     div_x.appendChild(para);
+    div_x.id=key;
+    //delete button
     var delbut = document.createElement("button");
+    delbut.id=key+"2";
     var del_text=document.createTextNode("Completed");
-    delbut.id=key;
     delbut.className="del_button";
     delbut.appendChild(del_text);
-    delbut.onclick= function (e) {  delete_task(e.target.id); };
+    delbut.onclick= function (e) {delete_task(e.target.parentElement.id); };
+
+    //edit button
+    var editbut = document.createElement("button");
+    editbut.id=key+"3";
+    var edit_text=document.createTextNode("edit");
+    editbut.className="del_button";
+    editbut.appendChild(edit_text);
+    editbut.onclick=function (e){ edit(e.target.parentElement.id); };
+
+    div_x.appendChild(editbut);
     div_x.appendChild(delbut);
     div_x.className="task_element";
     tasklist.appendChild(div_x);
