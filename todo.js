f50cc39caf3eb8b8eb6f66504ff773358c0eb15f
Vwore <v.wore@iitg.ac.in>
2023-07-22 10:28:24 +0530

    date

--- todo.js
+++ todo.js
@@ -13,8 +13,10 @@ function submits(e){
 	e.preventDefault();
     if(document.getElementById('one').value == "") {alert("Please enter the task");}
     else
-    {   tasks.set(curid,document.getElementById('one').value);
-        create_task(document.getElementById('one').value,curid);
+    {   
+        tasks.set(curid,[document.getElementById('one').value,document.getElementById('date').value]);
+        // console.log((tasks.get(curid))[1]);
+        create_task(tasks.get(curid),curid);
         document.getElementById('one').value = "";
         curid++;
     }
@@ -24,7 +26,9 @@ function edit_submit(e){
     e.preventDefault();
     var x=document.getElementById('editinput');
     // console.log(e.target.parentElement.id);
-    tasks.set((Number)(e.target.parentElement.id),x.value);
+    var y=tasks.get((Number)(e.target.parentElement.id));
+    y[0]=x.value;
+    tasks.set((Number)(e.target.parentElement.id),y);
     rerender();
 }
 
@@ -38,27 +42,38 @@ function edit(id)
 {
     // console.log('edit '+id);
     var x=document.getElementById(id);
-    x.innerHTML="<form id='editform'><label>Enter the event</label><input id='editinput'></input><input type='submit' value='SAVE' style='background-color: rgb(4, 105, 114); height: 35px; border-radius: 20px; color: white;' ></form>"; 
+    var y=document.getElementById(id+'1').textContent;
+    console.log(y);
+    x.innerHTML="<form id='editform'><label>Enter the event</label><input id='editinput' }></input><input type='submit' value='SAVE' style='background-color: rgb(4, 105, 114); height: 35px; border-radius: 20px; color: white;' ></form>"; 
     var edit_task=document.getElementById('editform');
     edit_task.addEventListener('submit',edit_submit);
 }
 
 function create_task(value,key)
 {
-    
+    console.log(value[1]);
     const tasklist=document.getElementById("task_list");
     var div_x=document.createElement("div");
+    var div_y=document.createElement("div");
+    div_y.className="task_div";
+
     var para= document.createElement("p");
-    var text= document.createTextNode(value);
+    var text= document.createTextNode(value[0]);
     para.className="task_text";
     para.id=key+"1";
     para.appendChild(text);
-    div_x.appendChild(para);
+    div_y.appendChild(para);
+    var date=document.createElement("p");
+    var date_text=document.createTextNode(value[1]);
+    date.appendChild(date_text);
+    date.style="font-size: 15px";
+    div_y.appendChild(date);
+    div_x.appendChild(div_y);
     div_x.id=key;
     //delete button
     var delbut = document.createElement("button");
     delbut.id=key+"2";
-    var del_text=document.createTextNode("Completed");
+    var del_text=document.createTextNode("delete");
     delbut.className="del_button";
     delbut.appendChild(del_text);
     delbut.onclick= function (e) {delete_task(e.target.parentElement.id); };
@@ -78,6 +93,7 @@ function create_task(value,key)
 }
 function rerender()
 {
+    console.log(typeof tasks);
     document.getElementById('task_list').innerHTML="";
     tasks.forEach(function(value,key) 
     {
